741d27249cd297b16c51941e61d63b5aaf09376e
mattvioli <mattvioli@gmail.com>
2021-09-05 16:53:54 +1000

    works on andriod emu

--- app/src/screens/AuthScreen.js
+++ app/src/screens/AuthScreen.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { ImageBackground, View, Text, StyleSheet, TouchableOpacity, TextInput, Platform } from 'react-native';
 
-const API_URL = 'http://localhost:5000' 
+const API_URL = Platform.OS === 'ios' ? 'http://localhost:5000' : 'http://10.0.2.2:5000';
 
 const AuthScreen = () => {
 
